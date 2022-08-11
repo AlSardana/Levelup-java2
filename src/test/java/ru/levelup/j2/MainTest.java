512401c8fd225ca101fa180f84ca27a865b6f7fe
User <sardanee@gmail.com>
2022-08-12 01:26:35 +0900

    Initial commit

---
+++ src/test/java/ru/levelup/j2/MainTest.java
@@ -0,0 +1,23 @@
+package ru.levelup.j2;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class MainTest extends TestCase {
+    @Test
+    public void testSum1() {
+        if (Main.sum(1,2) != 3)
+            fail("SUM of 1 and 2 should be 3");
+    }
+
+    @Test
+    public void testSum2() {
+    }
+    @Test
+    public void testSum3() {
+    }
+
+    @Test
+    public void sum() {
+    }
+}
